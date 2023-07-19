17b5c30b983c410639f4e87f8eab2b41059cf243
Thiago Correa da Fonseca <thiagocorreafonseca@gmail.com>
2023-07-18 23:16:21 -0300

    inclusao de metodos dos verbos HTTP para testes

--- DevFreela.Api/Controllers/ProjectsController.cs
+++ DevFreela.Api/Controllers/ProjectsController.cs
@@ -1,3 +1,4 @@
+using DevFreela.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.Api.Controllers
@@ -6,9 +7,44 @@ namespace DevFreela.Api.Controllers
     public class ProjectsController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
         }
     }
 }
